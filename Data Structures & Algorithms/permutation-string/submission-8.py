10ccae555a0a255da356a76e615208439a112ed1
CodyGargar <147003793+CodyGargar@users.noreply.github.com>
2026-07-13 20:40:04 -0700

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if( len(s1) > len(s2)):
+            return False
+        s1 = "".join(sorted(s1))
+        for x in range(len(s2) - len(s1)+1):
+            if s1 == "".join(sorted(s2[x: x + len(s1)])):
+                return True
+
+        return False
